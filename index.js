151009b56b0cfc2de30acb2f0f6e5249db4c4b2f
ThaiDevv <tranvanthai20062@gmai.com>
2025-08-17 18:21:47 +0700

    add sound and UI basic

---
+++ index.js
@@ -0,0 +1,34 @@
+document.querySelectorAll(".drum > div").forEach(element => {
+    element.addEventListener("click",() => playMusic(element.className))
+});
+function playMusic(drumType){
+    switch (drumType) {
+    case "snare":
+      sound = src="./sounds/snare.mp3";
+      break;
+    case "kick":
+      sound = "./sounds/kick.mp3";
+      break;
+    case "hi-hat":
+      sound = "./sounds/hihat.mp3";
+      break;
+    case "tom1":
+      sound = "./sounds/tom1.mp3";
+      break;
+    case "tom2":
+      sound = "./sounds/tom2.mp3";
+      break;
+    case "floor-tom":
+      sound = "./sounds/floortom.mp3";
+      break;
+    case "crash":
+      sound = "./sounds/crash.mp3";
+      break;
+    case "ride":
+      sound = "./sounds/ride.mp3";
+      break;
+    default:
+      console.log("Không có âm thanh cho:", drumType);
+    }
+    new Audio(sound).play();
+}
